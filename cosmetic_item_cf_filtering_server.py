72a5cd118a8645cf299cd60a8776ad7ab1b7dd08
maerad7 <maerad7@naver.com>
2022-08-23 22:58:53 +0900

    ML+DL

--- cosmetic_item_cf_filtering_server.py
+++ cosmetic_item_cf_filtering_server.py
@@ -5,6 +5,7 @@ import json
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+import tensorflow as tf
 
 app = Flask(__name__)
 # csv 로딩
@@ -21,8 +22,8 @@ df['item_to_replace_id'] = df["item_id"].map(lambda x:item_to_replace_id[x])
 df['user_replace_id'] = df['user_id'].map(lambda x:user_to_replace_id[x])
 
 # 피부 정보들
-skin_type_dict = { "1": '건성' , "2" :'지성', "3":'중성', '4':'수분부족지성', "5":'복합성', "6":'극건성'}
-skin_info_dict = { "1":'민감성', "2" :'모공', "3":'탄력없음', "4":'칙칙함', '5':'트러블', "6":'건조함', "7":'주름'}
+skin_type_dict = {0:'건성'  , 1:'지성', 2:'중성', 3:'수분부족지성', 4:'복합성', 5:'극건성'}
+skin_info_dict = {0:'민감성', 1:'모공', 2:'탄력없음', 3:'칙칙함', 4:'트러블', 5:'건조함', 6:'주름'}
 
 
 # 필요한 칼럼만 뽑아서 result_df 에 저장
@@ -39,6 +40,47 @@ index_to_item_id_dict = {}
 for item_name,item_id in zip(ratings.item_id.unique(),ratings.item_to_replace_id.unique()):
     index_to_item_id_dict[item_id] = item_name
 
+# deep_learning =====================
+
+# 아이템 전체 리스트
+item_list = df.item_to_replace_id.unique()
+
+# 딥러닝 모델 로드
+model = tf.keras.models.load_model('experiment_no_onehot_add_category_05.h5')
+
+def index_to_item_id(result_list):
+    item_ids = []
+    for result in result_list:
+        item_id = index_to_item_id_dict[result]
+        item_ids.append(int(item_id))
+    return item_ids
+
+# 딥러닝 모델 호출
+def model_predict(recommendation_model, user_id,item_id,skin_type,skin_info,age_group,gender,category,recommend_data_nums):
+    user = [user_to_replace_id[user_id]] *len(item_list)
+    skin_type = [skin_type]*len(item_list)
+    skin_info = [skin_info]*len(item_list)
+    age_group = [age_group]*len(item_list)
+    # gender = 1 if gender == "남성" else 0
+    gender = [gender]*len(item_list)
+    category =[category]*len(item_list)
+    user = np.array(user)
+    skin_type = np.array(skin_type)
+    skin_info = np.array(skin_info)
+    age_group = np.array(age_group)
+    gender = np.array(gender)
+    category = np.array(category)
+
+    prediction = recommendation_model.predict([user,item_id,skin_type,skin_info,age_group,gender,category])
+    result_list = get_recommendation_result(recommend_data_nums,prediction)
+    int_result_list = []
+    for i in result_list:
+        int_result_list.append(int(i))
+    result = index_to_item_id(int_result_list)     
+    res_data = {"recommend_list" : result }
+    return res_data
+
+
 def Item_base_cos_df(ratings):
     item_based = ratings.pivot_table('user_rating',index="item_to_replace_id",columns="user_replace_id")
     item_based.fillna(0,inplace=True)
@@ -53,12 +95,7 @@ def Item_base_cos_df(ratings):
 def recommend(sim_rate_df,item_id,k):
     return sim_rate_df[item_id].sort_values(ascending=False)[1:k+1]
 
-def index_to_item_id(result_list):
-    item_ids = []
-    for result in result_list:
-        item_id = index_to_item_id_dict[result]
-        item_ids.append(int(item_id))
-    return item_ids
+
 
 def Item_based_filtering(df,parmas_skin_type,params_skin_info,item_name,recommend_data_nums):
     item_id = item_to_index_dict[item_name] 
@@ -69,23 +106,54 @@ def Item_based_filtering(df,parmas_skin_type,params_skin_info,item_name,recommen
     item_id_list = index_to_item_id(result_list.index)
     return item_id_list
 
+def get_recommendation_result(k, prediction):
+    result = tf.nn.top_k(prediction.reshape(397), k=k).indices
+    result = list(result.numpy())
+    return result
+
 @app.route('/predict', methods=['GET','POST']) 
 def predict():
     json_request=None
+    send_data={}
     if request.method == "POST":
-        data = request.json
-        user_skin_type = data['skin_type']
-        user_skin_info = data['skin_info']
-        print(user_skin_type,user_skin_type)
+        try:
+            data = request.json
+            user_skin_type = data['skin_type']
+            user_skin_info = data['skin_info']
+            print(user_skin_type,user_skin_type)
 
-        item_name = data['item_name']
-        recommend_data_nums = data['recommend_data_nums']
-        user_skin_type = skin_type_dict[user_skin_type]
-        user_skin_info = skin_info_dict[user_skin_info]
-        print(user_skin_type,user_skin_info)
-        prediction = Item_based_filtering(df,user_skin_type,user_skin_info,item_name,recommend_data_nums)
-        send_data = {"data": prediction}
-        
+            item_name = data['item_name']
+            recommend_data_nums = data['recommend_data_nums']
+            user_skin_type = skin_type_dict[user_skin_type]
+            user_skin_info = skin_info_dict[user_skin_info]
+            # print(user_skin_type,user_skin_info)
+            prediction = Item_based_filtering(df,user_skin_type,user_skin_info,item_name,recommend_data_nums)
+            send_data = {"recommend_list": prediction}
+        except:
+            # 추천 리스트가 없습니다.
+            send_data = {"recommend_list": None}
+    return jsonify(send_data)
+
+@app.route('/deep_learning_predict', methods=['GET','POST']) 
+def deep_learning_predict():
+    json_request=None
+    send_data={}
+    
+    if request.method == "POST":
+        try:   
+            data = request.json
+            user_id = data['user_id']
+            skin_type = data['skin_type']
+            skin_info = data['skin_info']
+            age_group = data['age_group']/10 
+            gender = data['gender'] # 남성 1 여성 0
+            category = data['category']
+            recommend_data_nums = data['recommend_data_nums']
+
+            send_data = model_predict(model,user_id,item_list,skin_type,skin_info,age_group,gender,category,recommend_data_nums)
+        except:
+            # 추천 리스트가 없습니다.
+            send_data = {"recommend_list": None}
     return jsonify(send_data)
 
 if __name__ == "__main__":
